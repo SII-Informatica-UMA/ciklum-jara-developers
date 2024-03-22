132f3970bbfc1cb163a832b1066a88f7916b9d3a
javimp2003uma <javimp2003uma@uma.es>
2024-03-22 01:06:01 +0100

    newFiles

--- microservicioEntrenadores/src/app/welcome/script.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('cambiarTexto').addEventListener('click', function() {
-        document.getElementById('textoCambiado').innerText = '¡El texto ha sido cambiado!';
-    });
-});
