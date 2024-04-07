f261c6514083266ed4e2e61ad61dd60b83221e68
rociogzm22 <125653371+rociogzm22@users.noreply.github.com>
2024-04-07 13:50:10 +0200

    Implementación inicial de las pruebas

--- microServicio_entrenadores_sinBackend/src/app/app.component.spec.ts
+++ microServicio_entrenadores_sinBackend/src/app/app.component.spec.ts
@@ -1,8 +1,14 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, tick } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { Centro } from './centro';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Entrenador } from './entrenador';
 
 describe('AppComponent', () => {
+
+  let modalServiceStub: Partial<NgbModal>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
@@ -20,16 +26,67 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'practica5'`, () => {
+  it(`should initialize with empty values`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.contactoElegido).toBeUndefined();
+    expect(app.gerentes_entrenadores.size).toBe(0);
+    expect(app.centroElegido).toBeUndefined();
+    expect(app.gerenteElegido).toBeUndefined();
+    expect(app.entrenadorElegido).toBeUndefined();
   });
 
-  it('should render title', () => {
+  it('should select a center', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.container h1')?.textContent).toContain('Lista de contactos');
+    const app = fixture.componentInstance;
+    const centroMock: Centro = { nombre: "McFit", direccion: "Centro Comercial Carrefour Alameda", idCentro: 1,
+                                  rutaImagen: "https://www.abba.es/wp-content/uploads/2023/03/EKFKDpia.png",
+                                  anoFundacion: "2012"};
+    app.elegirCentro(centroMock);
+    expect(app.centroElegido).toEqual(centroMock);
   });
+
+  it('should select a trainer', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const entrenadorMock: Entrenador = { idUsuario: 4, direccion: "Calle Principal 123",
+                                          dni: "12345678A", fechaNacimiento: new Date("1990-05-15"), fechaAlta: new Date("2023-01-10"),
+                                          fechaBaja: new Date("2023-12-31"), especialidad: "Entrenamiento de fuerza",
+                                          titulacion: "Licenciado en Ciencias del Deporte", experiencia: "5 años de experiencia como entrenador personal",
+                                          observaciones: "Paciente y comprometido con el progreso de los clientes", id: 1 };
+    app.elegirEntrenador(entrenadorMock);
+    expect(app.entrenadorElegido).toEqual(entrenadorMock);
+  });
+
+  it('should initialize the map', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    app.ngOnInit();
+    expect(app.gerentes_entrenadores.size).toBe(5);
+    
+  })
+
+  it('should delete a selected trainer', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    app.ngOnInit();
+
+    const centroMock: Centro = { nombre: "McFit", direccion: "Centro Comercial Carrefour Alameda", idCentro: 1,
+                                  rutaImagen: "https://www.abba.es/wp-content/uploads/2023/03/EKFKDpia.png",
+                                  anoFundacion: "2012"};
+    app.elegirCentro(centroMock);
+    app.gerenteElegido = { idUsuario: 2, empresa: "McFit", id: 1};
+    const entrenadorMock: Entrenador = { idUsuario: 4, direccion: "Calle Principal 123",
+                                      dni: "12345678A", fechaNacimiento: new Date("1990-05-15"), fechaAlta: new Date("2023-01-10"),
+                                      fechaBaja: new Date("2023-12-31"), especialidad: "Entrenamiento de fuerza",
+                                      titulacion: "Licenciado en Ciencias del Deporte", experiencia: "5 años de experiencia como entrenador personal",
+                                      observaciones: "Paciente y comprometido con el progreso de los clientes", id: 1 };
+
+    expect(app.getEntrenadoresDelCentroElegido()?.length).toBe(2);
+
+    app.elegirEntrenador(entrenadorMock);
+    app.eliminarEntrenador(entrenadorMock.id);
+    expect(app.getEntrenadoresDelCentroElegido()?.length).toBe(1);
+  })
 });
